322f76fa02f04bf1817b359907c667e55148dd16
DannyFabs <aderemifawoyed@gmail.com>
2024-09-23 23:35:06 -0500

    linte test

---
+++ Backend/src/index.js
@@ -0,0 +1,11 @@
+var xyz = 10;
+
+function example(args = 1) {
+    if (args == 1) {
+        console.log("hello");
+    } else if (args == 2) {
+        console.log("world");
+    } else {
+        const a = "getcooked";
+    }
+}
