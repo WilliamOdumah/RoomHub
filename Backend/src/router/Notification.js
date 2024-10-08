1e37996fac32fdd5cb32c481594d198792384f5e
DannyFabs <aderemifawoyed@gmail.com>
2024-10-08 16:47:40 -0500

    updated documentation for notification/join-room-request

--- Backend/src/router/Notification.js
+++ Backend/src/router/Notification.js
@@ -25,6 +25,19 @@ router.post("/create-room-invite", (req, res) => {
     notification_handler.create_notification(req, res);
 });
 
+/**
+ * @memberof Notification
+ * @name Add a new notification
+ * @path {POST} notification/create-room-invite
+ * @query {String} from The sender user ID
+ * @query {String} to The receiver user ID
+ * @query {String} type The type of notification
+ * @code {200} Notification Successfully created
+ * @code {404} Error Creating Notification - User not found
+ * @code {400} Error Creating Notification - Message is empty
+ * @code {500} Backend error from the database
+ * @response {String} message See description of the different status codes
+ */
 router.post("/join-room-request", (req, res) => {
     notification_handler.create_notification(req, res);
 });
